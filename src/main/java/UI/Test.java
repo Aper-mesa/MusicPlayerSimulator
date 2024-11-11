0968911982e7ed37b6fe21dd961bf638b0d180ae
邹上豪 <zoush@kean.edu>
2024-11-11 16:38:18 +0800

    player bar

--- src/main/java/UI/Test.java
+++ src/main/java/UI/Test.java
@@ -6,6 +6,7 @@ import javafx.scene.Cursor;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
@@ -31,12 +32,59 @@ public class Test extends Application {
 
         VBox playPage = new VBox();
 
-        Image playIcon = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/icons/playButton.png")));
-        Image hoverPlayIcon = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/icons/playButtonHover.png")));
+        Image playIcon = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/icons/play.png")));
+        Image hoverPlayIcon = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/icons/playHover.png")));
 
-        Image downloadIcon = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/icons/downloadButton.png")));
-        Image hoverDownloadIcon = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/icons/downloadButtonHover.png")));
+        Image downloadIcon = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/icons/download.png")));
+        Image hoverDownloadIcon = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/icons/downloadHover.png")));
 
+        showSongs(playIcon, hoverPlayIcon, downloadIcon, hoverDownloadIcon, playPage);
+
+        VBox playerBar = new VBox(10);
+        playerBar.setStyle("-fx-background-color: #f0f0f0; -fx-padding: 10;");
+
+        HBox controls = new HBox(10);
+        controls.setAlignment(Pos.CENTER);
+
+        Image prevIcon = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/icons/previous.png")));
+        Image hoverPrevIcon = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/icons/previousHover.png")));
+        Image nextIcon = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/icons/next.png")));
+        Image hoverNextIcon = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/icons/nextHover.png")));
+
+        Button prevButton = getButton(prevIcon, hoverPrevIcon);
+        Button playPauseButton = new Button("Play/Pause");
+        Button nextButton = getButton(nextIcon, hoverNextIcon);
+        Button modeButton = new Button("Mode");
+
+        Region spacer1 = new Region();
+        Region spacer2 = new Region();
+        HBox.setHgrow(spacer1, Priority.ALWAYS);
+        HBox.setHgrow(spacer2, Priority.ALWAYS);
+
+        controls.getChildren().addAll(spacer1, prevButton, playPauseButton, nextButton, modeButton, spacer2);
+
+        ProgressBar progressBar = new ProgressBar(0.0);
+        progressBar.setPrefWidth(400);
+        HBox progressBarContainer = new HBox(progressBar);
+        progressBarContainer.setAlignment(Pos.CENTER);
+
+        playerBar.getChildren().addAll(controls, progressBarContainer);
+
+        BorderPane root = new BorderPane();
+        root.setTop(topBar);
+        root.setCenter(playPage);
+        root.setBottom(playerBar);
+
+        downloadPageButton.setOnAction(event -> root.setCenter(downloadPage));
+        playPageButton.setOnAction(event -> root.setCenter(playPage));
+
+        Scene scene = new Scene(root, 800, 600);
+        primaryStage.setScene(scene);
+        primaryStage.setResizable(false);
+        primaryStage.show();
+    }
+
+    private void showSongs(Image playIcon, Image hoverPlayIcon, Image downloadIcon, Image hoverDownloadIcon, VBox playPage) {
         for (int i = 1; i <= 5; i++) {
             HBox songRow = new HBox(10);
             songRow.setPrefHeight(50);
@@ -52,7 +100,7 @@ public class Test extends Application {
 
             Region spacer = new Region();
             spacer.setMinWidth(Region.USE_COMPUTED_SIZE);
-            HBox.setHgrow(spacer, javafx.scene.layout.Priority.ALWAYS);
+            HBox.setHgrow(spacer, Priority.ALWAYS);
 
             Button playButton = getButton(playIcon, hoverPlayIcon);
             Button downloadButton = getButton(downloadIcon, hoverDownloadIcon);
@@ -63,18 +111,6 @@ public class Test extends Application {
             songRow.setOnMouseEntered(event -> songRow.setStyle("-fx-background-color: #ececec;"));
             songRow.setOnMouseExited(event -> songRow.setStyle("-fx-background-color: transparent;"));
         }
-
-        BorderPane root = new BorderPane();
-        root.setTop(topBar);
-        root.setCenter(playPage);
-
-        downloadPageButton.setOnAction(event -> root.setCenter(downloadPage));
-        playPageButton.setOnAction(event -> root.setCenter(playPage));
-
-        Scene scene = new Scene(root, 800, 600);
-        primaryStage.setScene(scene);
-        primaryStage.setResizable(false);
-        primaryStage.show();
     }
 
     @NotNull
