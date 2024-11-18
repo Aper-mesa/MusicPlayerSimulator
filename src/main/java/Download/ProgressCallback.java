5ac3a1f43226a020197ba8feea861df2344fd774
邹上豪 <zoush@kean.edu>
2024-11-19 00:01:26 +0800

    delete all

--- src/main/java/Download/ProgressCallback.java
+++
@@ -1,8 +0,0 @@
-package Download;
-
-public interface ProgressCallback {
-    void updateProgress(double progress); // 更新进度
-    void onError(Exception e); // 处理错误
-    void onCancelled(); // 通知任务已取消
-    void onComplete(); // 通知任务已完成
-}
